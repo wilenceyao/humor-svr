8f9edffd8e9d9ea37996a75a08b6672a8b29f3af
wilenceyao <wilenceyao@tencent.com>
2021-01-03 23:16:45 +0800

    fix: emq-client.MqttPublish传参错误

--- pkg/emq-client/mqtt_publish.go
+++ pkg/emq-client/mqtt_publish.go
@@ -2,6 +2,6 @@ package emq_client
 
 func (c *EmqClient) MqttPublish(req *MqttPublishRequest) (*MqttPublishResponse, error) {
 	res := &MqttPublishResponse{}
-	err := c.post("/api/v4/mqtt/publish", res, req)
+	err := c.post("/api/v4/mqtt/publish", req, res)
 	return res, err
 }
